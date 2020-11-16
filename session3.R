672b6176c64fc9a12fd5c0ef5f36982c2da32d46
prabhdeep-dadyal <deepdadyal97@outlook.com>
2020-11-16 18:35:59 +0530

    session3

---
+++ session3.R
@@ -0,0 +1,34 @@
+#data Structures in R
+
+#way of storing and manipulating data in any programming language
+
+#vector----
+x = c(1,2,3,6)
+#vector for single data type
+x #print x
+x2 = c('a','b','Dhiraj') #character vector
+x2
+class(x2)
+?c #help
+mean(x) #mean of x vector values
+sum(x)
+x3 = 1:100
+x3
+x4 = seq(1,100,3) #Sequence
+x4
+x5 = list(1,3,5,'a')
+x5
+class(x5)
+#data Types - numeric, integer, character, logical
+x6 = c(T,TRUE,F,FALSE,F)
+length(x6)
+x6
+class(x6)
+#to print and create a vector together use rounded brackets
+
+#matrix----
+
+
+
+
+
